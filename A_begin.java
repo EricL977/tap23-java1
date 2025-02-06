b8b0269951a29c9007b651e4da1640e5c0f4b012
EricL977 <Eric.Ly@aig.com>
2025-02-06 16:26:26 +0000

    Class files

---
+++ A_begin.java
@@ -0,0 +1,29 @@
+/*
+ * Hello world
+ *  * Create integer, double, float
+ * Show the java repl - jshell
+ * show how System.out.println(a) with lowercase system fails
+ * Create function p()
+ * Prints a line
+ * 
+ */
+
+ public class A_begin{
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world!");
+
+        int myNumer;
+
+        myNumer = 5;
+
+        System.out.println(myNumer);
+
+        String str = "MyNumber";
+        p(str + ":" + myNumer);
+    }
+
+    public static void p(Object line){
+        System.out.println(line);
+    }
+ }
